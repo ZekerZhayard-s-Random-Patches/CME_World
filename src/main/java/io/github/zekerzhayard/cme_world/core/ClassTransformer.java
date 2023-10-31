70dd15875ae31b6ae13926be4b809a9cde8a5840
ZekerZhayard <trees1000@qq.com>
2023-10-31 15:00:48 +0800

    1.2

--- src/main/java/io/github/zekerzhayard/cme_world/core/ClassTransformer.java
+++ src/main/java/io/github/zekerzhayard/cme_world/core/ClassTransformer.java
@@ -42,6 +42,25 @@ public class ClassTransformer implements IClassTransformer {
             ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
             cn.accept(cw);
             basicClass = cw.toByteArray();
+        } else if ("net.minecraft.world.chunk.Chunk".equals(transformedName)) {
+            ClassNode cn = new ClassNode();
+            new ClassReader(basicClass).accept(cn, ClassReader.EXPAND_FRAMES);
+            for (MethodNode mn : cn.methods) {
+                if (RemapUtils.checkMethodName(cn.name, mn.name, mn.desc, "<init>") && RemapUtils.checkMethodDesc(mn.desc, "(Lnet/minecraft/world/World;II)V")) {
+                    for (AbstractInsnNode ain : mn.instructions.toArray()) {
+                        if (ain.getOpcode() == Opcodes.PUTFIELD) {
+                            FieldInsnNode fin = (FieldInsnNode) ain;
+                            if (RemapUtils.checkClassName(fin.owner, "net/minecraft/world/chunk/Chunk") && RemapUtils.checkFieldName(fin.owner, fin.name, fin.desc, "field_150816_i") && RemapUtils.checkFieldDesc(fin.desc, "Ljava/util/Map;")) {
+                                mn.instructions.insertBefore(fin, new InsnNode(Opcodes.POP));
+                                mn.instructions.insertBefore(fin, new MethodInsnNode(Opcodes.INVOKESTATIC, "io/github/zekerzhayard/cme_world/ConcurrentHashMapWithNullKey", "create", "()Ljava/util/Map;", false));
+                            }
+                        }
+                    }
+                }
+            }
+            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+            cn.accept(cw);
+            basicClass = cw.toByteArray();
         }
         return basicClass;
     }
